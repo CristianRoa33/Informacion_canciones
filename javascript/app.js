99fdc9353af946e3db562392e8fae11af383b4dd
CristianRoa33 <ccroa33@gmail.com>
2026-05-03 12:48:26 -0500

    codificación

---
+++ javascript/app.js
@@ -0,0 +1,64 @@
+
+/*Codigo que resuelve la pregunta */
+
+const personas = []
+
+function datosPersonas () {
+    if (personas.length === 6) {
+        console.log("Ya hay 6 personas registradas")
+    } else {
+       /*  const nombre = prompt("Ingrese el nombre de la persona")
+        const cedula = prompt("Ingrese la cedula de la persona")
+        const fechaNacimiento = prompt("Ingrese la fecha de nacimiento de la persona")
+        const ciudadResidencia = prompt("Ingrese la ciudad de residencia de la persona")
+        const ciudadOrigen = prompt("Ingrese la ciudad de origen de la persona") */
+        let persona= {
+            nombre: nombre,
+            cedula: cedula,
+            fechaNacimiento: fechaNacimiento,
+            ciudadResidencia: ciudadResidencia,
+            ciudadOrigen: ciudadOrigen,
+            canciones: []
+        }
+
+        for (let i = 0; i < 3; i++) {
+          /*   const artista = prompt("Ingrese el nombre del artista")
+            const cancion = prompt("Ingrese el nombre de la canción")
+            const genero = prompt("Ingrese el género de la canción")
+ */
+            if (artista === "" || artista === null) {
+                break
+                console.log("No se ingresó una canción válida")
+        }
+
+            persona.canciones.push({
+                cancion: cancion,
+                artista: artista,
+                genero: genero
+            })
+        }
+
+        personas.push(persona)
+        console.log(persona)
+
+    }
+      
+}
+
+console.log(datosPersonas())
+
+function rifa() {
+    if (personas.length < 6) {
+        console.log("Todavía no hay 6 personas registradas")
+        return
+    }
+
+    // Generar número aleatorio entre 0 y 5
+    let min = 0
+    let max = personas.length - 1
+    let indiceGanador = Math.floor(Math.random() * (max - min + 1)) + min
+
+    // Acceder al objeto ganador
+    let ganador = personas[indiceGanador]
+
+}
